e805e393e3e2005abea1cecdfee91acf18dfdbf8
Dusty Kennedy <dustyken@yahoo.com>
2022-08-05 21:33:40 -0500

    Update streamlit_app.py
    cleaned up fruityvice data and put in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# next line normalizes the data from fruityvice 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# puts normalized data in a dataframe
+streamlit.dataframe(fruityvice_normalized)
